d7b4ec9f5ca7c867e16adb70e1a26170b8acb1d5
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2024-09-04 17:19:51 +0200

    Logging

--- extension/script.js
+++ extension/script.js
@@ -5,60 +5,67 @@ localStorage.setItem('playlistId', id);
 
 // happens on click of the start button
 document.getElementById('start').addEventListener('click', function() {
-    const average = 0
-    const median = 0
-    const averageDuration = 0
-    const token = ""
-    let tokenTimer
-    const decades = []
-    
-    onMounted(() => {
-        fetchToken()
-    })
-    
-    async function fetchToken() {
-        const client_id = import.meta.env.VITE_CLIENT_ID
-        const client_secret = import.meta.env.VITE_CLIENT_SECRET
-        
-        await fetch("https://accounts.spotify.com/api/token", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`
-        })
-        .then(response => response.json())
-        .then(data => {
-            token = data.access_token
-            tokenTimer = new Date()
+    document.getElementById('start').textContent = 'Loading...';
+    let log = document.getElementById('log');
 
-            fetchTracks()
-        })
-    }
+    let average = 0
+    let median = 0
+    let averageDuration = 0
+    let token = ""
+    let decades = []
+    
+    let client_id = import.meta.env.VITE_CLIENT_ID
+    let client_secret = import.meta.env.VITE_CLIENT_SECRET
+
+    log.innerHTML = log.innerHTML + 'Fetching token...<br>';
+    
+    fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        token = data.access_token
+        log.innerHTML = log.innerHTML + 'Token fetched...<br>';
+        fetchTracks()
+    })
+    .catch(error => {
+        log.innerHTML = log.innerHTML + 'Error fetching token...<br>' + error + '<br>';
+    });
+
+    log.innerHTML = log.innerHTML + 'Between funcs...<br>';
     
     async function fetchTracks() {
-        const playlist_id = localStorage.getItem('playlistId')
+        let playlist_id = localStorage.getItem('playlistId');
         let tracks = []
         let nextUrl = `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`
-        
-        if (new Date() - tokenTimer > 600000) {
-            await fetchToken()
-        }
-        
+        log.innerHTML = log.innerHTML + 'Playlist ID: ' + playlist_id + '<br>';
+
+        log.innerHTML = log.innerHTML + 'Fetching tracks...<br>';
+
         while (nextUrl !== null) {
             await fetch(nextUrl, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
             })
-            .then(response => response.json())
+            .then(response => {log.innerHTML = log.innerHTML + JSON.stringify(response) + '<br>'; response.json()})
             .then(data => {
                 data.items.forEach(element => {
                     tracks.push(element.track)
                 });
                 nextUrl = data.next
             })
+            .catch(error => {
+                log.innerHTML = log.innerHTML + 'Error fetching token...<br>' + error + '<br>';
+            });
+            crash()
         }
+
+        log.innerHTML = log.innerHTML + 'Tracks fetched...<br>';
         
         let total = 0
         let releaseDates = []
@@ -86,36 +93,38 @@ document.getElementById('start').addEventListener('click', function() {
         average = Math.floor(total / tracks.length)
         median = releaseDates[Math.floor(releaseDates.length / 2)]
         averageDuration = Math.floor(averageDuration / tracks.length) / 1000
+
+        log.innerHTML = log.innerHTML + 'Tracks processed...<br>';
+        
+        let averageP = document.createElement('p');
+        p.textContent = `Average release year: ${average}`;
+        document.getElementById('test').appendChild(averageP);
+        let medianP = document.createElement('p');
+        p.textContent = `Average release year: ${median}`;
+        document.getElementById('test').appendChild(medianP);
+        
+        for (let i = 0; i < decades.length; i++) {
+            let decade = decades[i];
+            let decadeDiv = document.createElement('div');
+            decadeDiv.classList.add('decade');
+            let decadeP = document.createElement('p');
+            decadeP.textContent = `${decade.decade} (${decade.songs.length})`;
+            decadeDiv.appendChild(decadeP);
+            let songsDiv = document.createElement('div');
+            songsDiv.classList.add('songs');
+            decade.songs.forEach(song => {
+                let songP = document.createElement('p');
+                songP.textContent = `${song.name} - ${song.artists[0].name}`;
+                songsDiv.appendChild(songP);
+            });
+            decadeDiv.appendChild(songsDiv);
+            document.getElementById('test').appendChild(decadeDiv);
+        }
+        
+        let durationP = document.createElement('p');
+        p.textContent = `Average duration: ${averageDuration}`;
+        document.getElementById('test').appendChild(durationP);
+        
+        document.getElementById('start').style.display = 'none';
     }
-
-    let averageP = document.createElement('p');
-    p.textContent = `Average release year: ${average}`;
-    document.getElementById('test').appendChild(averageP);
-    let medianP = document.createElement('p');
-    p.textContent = `Average release year: ${median}`;
-    document.getElementById('test').appendChild(medianP);
-
-    for (let i = 0; i < decades.length; i++) {
-        let decade = decades[i];
-        let decadeDiv = document.createElement('div');
-        decadeDiv.classList.add('decade');
-        let decadeP = document.createElement('p');
-        decadeP.textContent = `${decade.decade} (${decade.songs.length})`;
-        decadeDiv.appendChild(decadeP);
-        let songsDiv = document.createElement('div');
-        songsDiv.classList.add('songs');
-        decade.songs.forEach(song => {
-            let songP = document.createElement('p');
-            songP.textContent = `${song.name} - ${song.artists[0].name}`;
-            songsDiv.appendChild(songP);
-        });
-        decadeDiv.appendChild(songsDiv);
-        document.getElementById('test').appendChild(decadeDiv);
-    }
-
-    let durationP = document.createElement('p');
-    p.textContent = `Average duration: ${averageDuration}`;
-    document.getElementById('test').appendChild(durationP);
-
-    document.getElementById('start').style.display = 'none';
 });
