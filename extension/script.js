55fbcf458c641282a6ee5a41cfa8511eb864e8d3
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2024-09-04 14:49:43 +0200

    some scripting

--- extension/script.js
+++ extension/script.js
@@ -1,1 +1,11 @@
 console.log('Hello from script.js');
+
+document.getElementById('test').innerHTML = 'Hello from script.js';
+
+document.getElementById('start').addEventListener('click', run() );
+
+function run() {
+    console.log('run');
+    console.log(window.location)
+    document.getElementById('test').innerHTML = 'running...';
+}
